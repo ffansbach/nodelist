76a3cee4975e2992325d32e4ca853ea7112beaf6
StilgarBF <tino.dietel@projekt2k.de>
2015-04-21 11:08:46 +0200

    added schema and example

---
+++ examples/router_json.php
@@ -0,0 +1,510 @@
+{
+    "version": "1.0.0",
+    "updated_at": "2015-04-21T11:07:21+02:00",
+    "community": {
+        "name": "FreifunkEmskirchen",
+        "href": "https://raw.githubusercontent.com/ffansbach/community-files/master/emskirchen.json"
+    },
+    "nodes": [
+        {
+            "id": "1",
+            "name": "Sparkassenplatz8-hinten",
+            "node_type": "AccessPoint",
+            "href": "https://netmon.freifunk-emskirchen.de/router.php?router_id=1",
+            "status": {
+                "online": true,
+                "clients": 1,
+                "lastcontact": "2015-04-21T11:00:05+02:00"
+            },
+            "links": {
+                "admin-c": {
+                    "type": "people",
+                    "id": "2"
+                }
+            },
+            "position": {
+                "lat": "49.549516955195",
+                "long": "10.717663777727"
+            }
+        },
+        {
+            "id": "3",
+            "name": "Sparkassenplatz8",
+            "node_type": "AccessPoint",
+            "href": "https://netmon.freifunk-emskirchen.de/router.php?router_id=3",
+            "status": {
+                "online": true,
+                "clients": 1,
+                "lastcontact": "2015-04-21T11:00:08+02:00"
+            },
+            "links": {
+                "admin-c": {
+                    "type": "people",
+                    "id": "2"
+                }
+            },
+            "position": {
+                "lat": "49.549604000454",
+                "long": "10.717661375721"
+            }
+        },
+        {
+            "id": "4",
+            "name": "Aurachtreff-1",
+            "node_type": "AccessPoint",
+            "href": "https://netmon.freifunk-emskirchen.de/router.php?router_id=4",
+            "status": {
+                "online": true,
+                "clients": 0,
+                "lastcontact": "2015-04-21T11:00:10+02:00"
+            },
+            "links": {
+                "admin-c": {
+                    "type": "people",
+                    "id": "2"
+                }
+            },
+            "position": {
+                "lat": "49.550671733213",
+                "long": "10.715538571027"
+            }
+        },
+        {
+            "id": "5",
+            "name": "Antalya-Grill",
+            "node_type": "AccessPoint",
+            "href": "https://netmon.freifunk-emskirchen.de/router.php?router_id=5",
+            "status": {
+                "online": true,
+                "clients": 0,
+                "lastcontact": "2015-04-21T11:00:13+02:00"
+            },
+            "links": {
+                "admin-c": {
+                    "type": "people",
+                    "id": "2"
+                }
+            },
+            "position": {
+                "lat": "49.550357196603",
+                "long": "10.718140276968"
+            }
+        },
+        {
+            "id": "6",
+            "name": "Emskirchen",
+            "node_type": "AccessPoint",
+            "href": "https://netmon.freifunk-emskirchen.de/router.php?router_id=6",
+            "status": {
+                "online": true,
+                "clients": 0,
+                "lastcontact": "2015-04-21T11:00:17+02:00"
+            },
+            "links": {
+                "admin-c": {
+                    "type": "people",
+                    "id": "2"
+                }
+            },
+            "position": {
+                "lat": "49.549680871682",
+                "long": "10.717730143260"
+            }
+        },
+        {
+            "id": "7",
+            "name": "Cafe-Duenisch",
+            "node_type": "AccessPoint",
+            "href": "https://netmon.freifunk-emskirchen.de/router.php?router_id=7",
+            "status": {
+                "online": true,
+                "clients": 0,
+                "lastcontact": "2015-04-21T11:00:20+02:00"
+            },
+            "links": {
+                "admin-c": {
+                    "type": "people",
+                    "id": "2"
+                }
+            },
+            "position": {
+                "lat": "49.550566881032",
+                "long": "10.718244890308"
+            }
+        },
+        {
+            "id": "9",
+            "name": "Fahrschule-Claudia",
+            "node_type": "AccessPoint",
+            "href": "https://netmon.freifunk-emskirchen.de/router.php?router_id=9",
+            "status": {
+                "online": true,
+                "clients": 1,
+                "lastcontact": "2015-04-21T11:00:22+02:00"
+            },
+            "links": {
+                "admin-c": {
+                    "type": "people",
+                    "id": "2"
+                }
+            },
+            "position": {
+                "lat": "49.548967439826",
+                "long": "10.712683658006"
+            }
+        },
+        {
+            "id": "10",
+            "name": "Buergermeister3",
+            "node_type": "AccessPoint",
+            "href": "https://netmon.freifunk-emskirchen.de/router.php?router_id=10",
+            "status": {
+                "online": true,
+                "clients": 0,
+                "lastcontact": "2015-04-21T11:00:25+02:00"
+            },
+            "links": {
+                "admin-c": {
+                    "type": "people",
+                    "id": "3"
+                }
+            },
+            "position": {
+                "lat": "49.552219578979",
+                "long": "10.710117775432"
+            }
+        },
+        {
+            "id": "11",
+            "name": "Quattro-Stagioni",
+            "node_type": "AccessPoint",
+            "href": "https://netmon.freifunk-emskirchen.de/router.php?router_id=11",
+            "status": {
+                "online": true,
+                "clients": 1,
+                "lastcontact": "2015-04-21T11:00:27+02:00"
+            },
+            "links": {
+                "admin-c": {
+                    "type": "people",
+                    "id": "2"
+                }
+            },
+            "position": {
+                "lat": "49.550087595520",
+                "long": "10.717746926209"
+            }
+        },
+        {
+            "id": "12",
+            "name": "Wild-Busreisen",
+            "node_type": "AccessPoint",
+            "href": "https://netmon.freifunk-emskirchen.de/router.php?router_id=12",
+            "status": {
+                "online": true,
+                "clients": 0,
+                "lastcontact": "2015-04-21T11:00:30+02:00"
+            },
+            "links": {
+                "admin-c": {
+                    "type": "people",
+                    "id": "2"
+                }
+            },
+            "position": {
+                "lat": "49.554400069231",
+                "long": "10.704671914049"
+            }
+        },
+        {
+            "id": "13",
+            "name": "Cebramedia",
+            "node_type": "AccessPoint",
+            "href": "https://netmon.freifunk-emskirchen.de/router.php?router_id=13",
+            "status": {
+                "online": true,
+                "clients": 0,
+                "lastcontact": "2015-04-21T11:00:32+02:00"
+            },
+            "links": {
+                "admin-c": {
+                    "type": "people",
+                    "id": "4"
+                }
+            },
+            "position": {
+                "lat": "49.547212932117",
+                "long": "10.721255006561"
+            }
+        },
+        {
+            "id": "14",
+            "name": "LesBar",
+            "node_type": "AccessPoint",
+            "href": "https://netmon.freifunk-emskirchen.de/router.php?router_id=14",
+            "status": {
+                "online": true,
+                "clients": 1,
+                "lastcontact": "2015-04-21T11:00:34+02:00"
+            },
+            "links": {
+                "admin-c": {
+                    "type": "people",
+                    "id": "2"
+                }
+            },
+            "position": {
+                "lat": "49.550404833519",
+                "long": "10.713155726792"
+            }
+        },
+        {
+            "id": "15",
+            "name": "Aurachtreff-2",
+            "node_type": "AccessPoint",
+            "href": "https://netmon.freifunk-emskirchen.de/router.php?router_id=15",
+            "status": {
+                "online": true,
+                "clients": 0,
+                "lastcontact": "2015-04-21T11:00:37+02:00"
+            },
+            "links": {
+                "admin-c": {
+                    "type": "people",
+                    "id": "2"
+                }
+            },
+            "position": {
+                "lat": "49.550575238457",
+                "long": "10.715590940503"
+            }
+        },
+        {
+            "id": "16",
+            "name": "Goldener-Hirsch-unten",
+            "node_type": "AccessPoint",
+            "href": "https://netmon.freifunk-emskirchen.de/router.php?router_id=16",
+            "status": {
+                "online": true,
+                "clients": 0,
+                "lastcontact": "2015-04-21T11:00:39+02:00"
+            },
+            "links": {
+                "admin-c": {
+                    "type": "people",
+                    "id": "2"
+                }
+            },
+            "position": {
+                "lat": "49.550051172598",
+                "long": "10.718323629891"
+            }
+        },
+        {
+            "id": "18",
+            "name": "Fahrradwerkstatt-Loeb",
+            "node_type": "AccessPoint",
+            "href": "https://netmon.freifunk-emskirchen.de/router.php?router_id=18",
+            "status": {
+                "online": true,
+                "clients": 1,
+                "lastcontact": "2015-04-21T11:00:42+02:00"
+            },
+            "links": {
+                "admin-c": {
+                    "type": "people",
+                    "id": "2"
+                }
+            },
+            "position": {
+                "lat": "49.550343927865",
+                "long": "10.718675712944"
+            }
+        },
+        {
+            "id": "19",
+            "name": "Versorgungsspezialist",
+            "node_type": "AccessPoint",
+            "href": "https://netmon.freifunk-emskirchen.de/router.php?router_id=19",
+            "status": {
+                "online": true,
+                "clients": 0,
+                "lastcontact": "2015-04-21T11:00:05+02:00"
+            },
+            "links": {
+                "admin-c": {
+                    "type": "people",
+                    "id": "2"
+                }
+            },
+            "position": {
+                "lat": "49.550015040682",
+                "long": "10.716977867450"
+            }
+        },
+        {
+            "id": "20",
+            "name": "Prunklosia",
+            "node_type": "AccessPoint",
+            "href": "https://netmon.freifunk-emskirchen.de/router.php?router_id=20",
+            "status": {
+                "online": true,
+                "clients": 1,
+                "lastcontact": "2015-04-21T11:00:08+02:00"
+            },
+            "links": {
+                "admin-c": {
+                    "type": "people",
+                    "id": "2"
+                }
+            },
+            "position": {
+                "lat": "49.551227732943",
+                "long": "10.708253611420"
+            }
+        },
+        {
+            "id": "21",
+            "name": "Posthoernla",
+            "node_type": "AccessPoint",
+            "href": "https://netmon.freifunk-emskirchen.de/router.php?router_id=21",
+            "status": {
+                "online": false,
+                "clients": 0,
+                "lastcontact": "2015-02-12T16:09:22+01:00"
+            },
+            "links": {
+                "admin-c": {
+                    "type": "people",
+                    "id": "2"
+                }
+            },
+            "position": {
+                "lat": "49.549211068851",
+                "long": "10.719866613747"
+            }
+        },
+        {
+            "id": "22",
+            "name": "Goldener-Hirsch-oben",
+            "node_type": "AccessPoint",
+            "href": "https://netmon.freifunk-emskirchen.de/router.php?router_id=22",
+            "status": {
+                "online": true,
+                "clients": 0,
+                "lastcontact": "2015-04-21T11:00:10+02:00"
+            },
+            "links": {
+                "admin-c": {
+                    "type": "people",
+                    "id": "2"
+                }
+            },
+            "position": {
+                "lat": "49.550015040682",
+                "long": "10.718367251721"
+            }
+        },
+        {
+            "id": "24",
+            "name": "Goldener-Hirsch-Marktstrasse",
+            "node_type": "AccessPoint",
+            "href": "https://netmon.freifunk-emskirchen.de/router.php?router_id=24",
+            "status": {
+                "online": true,
+                "clients": 2,
+                "lastcontact": "2015-04-21T11:00:13+02:00"
+            },
+            "links": {
+                "admin-c": {
+                    "type": "people",
+                    "id": "2"
+                }
+            },
+            "position": {
+                "lat": "49.549872446005",
+                "long": "10.718741810832"
+            }
+        },
+        {
+            "id": "26",
+            "name": "Goldener-Hirsch-hinten",
+            "node_type": "AccessPoint",
+            "href": "https://netmon.freifunk-emskirchen.de/router.php?router_id=26",
+            "status": {
+                "online": true,
+                "clients": 0,
+                "lastcontact": "2015-04-21T11:00:17+02:00"
+            },
+            "links": {
+                "admin-c": {
+                    "type": "people",
+                    "id": "2"
+                }
+            },
+            "position": {
+                "lat": "49.549899990166",
+                "long": "10.718558665643"
+            }
+        },
+        {
+            "id": "27",
+            "name": "Schuler",
+            "node_type": "AccessPoint",
+            "href": "https://netmon.freifunk-emskirchen.de/router.php?router_id=27",
+            "status": {
+                "online": true,
+                "clients": 0,
+                "lastcontact": "2015-04-21T11:00:19+02:00"
+            },
+            "links": {
+                "admin-c": {
+                    "type": "people",
+                    "id": "2"
+                }
+            },
+            "position": {
+                "lat": "49.549825423761",
+                "long": "10.718756194533"
+            }
+        },
+        {
+            "id": "28",
+            "name": "Schuler-Hof",
+            "node_type": "AccessPoint",
+            "href": "https://netmon.freifunk-emskirchen.de/router.php?router_id=28",
+            "status": {
+                "online": true,
+                "clients": 1,
+                "lastcontact": "2015-04-21T11:00:22+02:00"
+            },
+            "links": {
+                "admin-c": {
+                    "type": "people",
+                    "id": "2"
+                }
+            },
+            "position": {
+                "lat": "49.549677098195",
+                "long": "10.718529882202"
+            }
+        }
+    ],
+    "linked": [
+        {
+            "id": "2",
+            "name": "StilgarBF",
+            "href": "https://netmon.freifunk-emskirchen.de/user.php?user_id=2"
+        },
+        {
+            "id": "3",
+            "name": "Christian",
+            "href": "https://netmon.freifunk-emskirchen.de/user.php?user_id=3"
+        },
+        {
+            "id": "4",
+            "name": "cebramedia",
+            "href": "https://netmon.freifunk-emskirchen.de/user.php?user_id=4"
+        }
+    ]
+}
